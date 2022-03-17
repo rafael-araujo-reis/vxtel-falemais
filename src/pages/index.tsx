39d2fb25b4a04a94aa2b47bb707a1821cbc27e94
Rafael Reis <rafa.araujoreis@gmail.com>
2022-03-17 06:03:00 -0300

    create project

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from './home.module.scss';
+
+export default function Home() {
+  return (
+    <h1>Hello word</h1>
+  );
+}
