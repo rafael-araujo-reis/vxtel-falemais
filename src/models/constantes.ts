3e54c8c9cc6a275accb29277aac5acd9acb66e80
Rafael Reis <rafa.araujoreis@gmail.com>
2022-03-17 22:44:35 -0300

    add items menu

---
+++ src/models/constantes.ts
@@ -0,0 +1,13 @@
+
+export const itemsMenu = [
+  {
+    id: 1,
+    title: 'Simular contratação',
+    url: '#simulateHiring'
+  },
+  {
+    id: 2,
+    title: 'Planos',
+    url: '#planos'
+  }
+];
