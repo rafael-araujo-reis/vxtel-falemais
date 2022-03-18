e02f94414def3a7f21f1b699da1e728db9e56008
Rafael Reis <rafa.araujoreis@gmail.com>
2022-03-17 22:43:05 -0300

    add fonts family

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,8 +5,10 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700;900&display=swap" rel="stylesheet" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Audiowide&family=Poppins:wght@400;600;900&display=swap" rel="stylesheet" />
 
           <link rel="shortcut icon" href="favicon.png" type="image/png" />
         </Head>
@@ -18,4 +20,4 @@ export default class MyDocument extends Document {
       </Html>
     );
   }
-}
+};;
