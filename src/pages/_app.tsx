301e650bf9fee1ddd4fd5ec0fae9a6778a5aae7c
Rafael Reis <rafa.araujoreis@gmail.com>
2022-03-18 23:05:44 -0300

    add providers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,15 @@
 import { AppProps } from 'next/app';
 import '../../styles/global.scss';
 import Header from '../components/Header';
+import { PlansProvider } from '../hooks/usePlans';
 import { itemsMenu } from '../models/constantes';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <PlansProvider>
       <Header itemsMenu={itemsMenu} />
       <Component {...pageProps} />
-    </>
+    </PlansProvider>
   );
 }
 
