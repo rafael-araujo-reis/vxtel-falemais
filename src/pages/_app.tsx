6c74dd13844e201882844231224b7a39edd4ac09
Rafael Reis <rafa.araujoreis@gmail.com>
2022-03-22 22:01:57 -0300

    change styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from 'next/app';
-import '../../styles/global.scss';
+import '../styles/global.scss';
 import Header from '../components/Header';
 import { PlansProvider } from '../hooks/usePlans';
 import { itemsMenu } from '../utils/constantes';
